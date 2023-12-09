db0ca44907e3fd5c8f398dd9bd93184df83ba616
themmj <49102245+themmj@users.noreply.github.com>
2023-12-09 18:03:42 +0000

    Enhance upstream based updates

--- pull-upstream.sh
+++ pull-upstream.sh
@@ -4,4 +4,14 @@ upstream_url="https://github.com/themmj/assignment-template.git"
 upstream_branch="main"
 
 git remote get-url upstream &> /dev/null || git remote add --fetch upstream $upstream_url
-git fetch upstream $upstream_branch && git rebase upstream/$upstream_branch
+rebase_error=$(git fetch upstream $upstream_branch &> /dev/null && git rebase upstream/$upstream_branch 2>&1)
+if [ $? -ne 0 ]; then
+    [[ "$rebase_error" =~ "unstaged changes" ]] &&
+        echo "unable to update: your local changes have not been commited"
+    [[ "$rebase_error" =~ "CONFLICT" ]] &&
+        echo "unable to update: there are merge conflicts, please contact your lecturer" &&
+        echo "if you know your way around with git you can attempt to rebase yourself from upstream/$upstream_branch" &&
+        git rebase --abort
+else
+    echo "you're now on the most recent version"
+fi
