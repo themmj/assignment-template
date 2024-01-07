1164b9cf272b29afc74cdd7305ef949022b23243
themmj <49102245+themmj@users.noreply.github.com>
2024-01-07 15:25:36 +0000

    Add bitwise printing helper

--- .assignment/dhbw.c
+++ .assignment/dhbw.c
@@ -13,3 +13,14 @@ void dhbw_print_line(const char* text) {
 void dhbw_print_float(const char* description, double v) {
     printf("%s: %.2lf\n", description, v);
 }
+
+void dhbw_print_bitwise_uint(const char *description, unsigned int v) {
+    for (int i = 31; i >= 0; --i) {
+        printf("%d", (v >> i) & 1);
+    }
+    printf(": %s\n", description);
+}
+
+void dhbw_print_bitwise_int(const char *description, int v) {
+    dhbw_print_bitwise_uint(description, (unsigned int)v);
+}
--- .assignment/dhbw.h
+++ .assignment/dhbw.h
@@ -31,3 +31,29 @@ void dhbw_print_line(const char* text);
  * @param v the actual float value
  */
 void dhbw_print_float(const char* description, double v);
+
+/**
+ * Print the bitwise representation of an unsigned integer starting from the MSB.
+ * 
+ * Example usage:
+ *  dhbw_print_bitwise_int("large number", 65000);
+ * The above would result in the following being printed into the console:
+ *  00000000000000001111110111101000: large number
+ * 
+ * @param description a formal description of the value to be printed
+ * @param v the actual unsigned integer value to print
+ */
+void dhbw_print_bitwise_uint(const char *description, unsigned int v);
+
+/**
+ * Print the bitwise representation of an integer starting from the MSB.
+ * 
+ * Example usage:
+ *  dhbw_print_bitwise_int("negative number", -4);
+ * The above would result in the following being printed into the console:
+ *  11111111111111111111111111111100: negative number
+ * 
+ * @param description a formal description of the value to be printed
+ * @param v the actual integer value to print
+ */
+void dhbw_print_bitwise_int(const char *description, int v);
